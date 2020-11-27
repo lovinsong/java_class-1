1637fc06e40555e33c59581ee9fce77f5ba121cf
H J Kwon <gurwns2030@gmail.com>
2020-11-27 17:47:56 +0900

    20/11/27 Swing 첫날

--- src/jdbc/HikariCpTest.java
+++ src/jdbc/HikariCpTest.java
@@ -1,9 +1,11 @@
 package jdbc;
 
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -11,15 +13,41 @@ import com.zaxxer.hikari.HikariDataSource;
 public class HikariCpTest {
 
 	public static void main(String[] args) {
+		// 1. HikariConfig 객체에 설정을 넣은 후 DataSource 생성자에 전달 
+//		HikariConfig config = new HikariConfig();
+//		config.setJdbcUrl("jdbc:oracle:thin:@localhost:1521/XEPDB1");
+//		config.setUsername("hr");
+//		config.setPassword("1234");
+//		config.addDataSourceProperty("cachePrepStmts", "true");
+//		config.addDataSourceProperty("prepStmtCacheSize", "250");
+//		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
 		
-		HikariConfig config = new HikariConfig();
-		config.setJdbcUrl("jdbc:oracle:thin:@localhost:1521/XEPDB1");
-		config.setUsername("hr");
-		config.setPassword("1234");
-		config.addDataSourceProperty("cachePrepStmts", "true");
-		config.addDataSourceProperty("prepStmtCacheSize", "250");
-		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-
+		// HikariDataSource ds = new HikariDataSource(config);		
+		
+		// 2. Config 만들기 귀찮으면 이렇게 하세요
+//		HikariDataSource ds = new HikariDataSource();
+//		ds.setJdbcUrl("jdbc:oracle:thin:@localhost:1521/XEPDB1");
+//		ds.setUsername("hr");
+//		ds.setPassword("1234");
+		
+		// 3. 설정 파일에 저장된 값을 이용하는 방법
+		
+		// Map, 딕셔너리, 속성, Property, Entry, JSON.. 
+		// 데이터를 Key=Value형태로 다루는 것들
+		
+		// ※ 프로젝트 내부의 경로를 찾아갈 때
+//		HikariConfig config = new HikariConfig("some/path/hikari.properties");
+//		HikariDataSource ds = new HikariDataSource(config);
+		
+		// 4. 설정 파일 객체를 생성해서 전달하는 방법
+		Properties props = new Properties();
+		props.setProperty("dataSourceClassName", "oracle.jdbc.pool.OracleDataSource");
+		props.setProperty("dataSource.url", "jdbc:oracle:thin:@localhost:1521/XEPDB1");
+		props.setProperty("dataSource.user", "hr");
+		props.setProperty("dataSource.password", "1234");		
+		props.put("dataSource.logWriter", new PrintWriter(System.out));
+		
+		HikariConfig config = new HikariConfig(props);
 		HikariDataSource ds = new HikariDataSource(config);
 		
 		try {
